7d21e594ef53629eff89c7f4f718840f2cea7eb4
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-09-13 19:17:37 +0300

    comment out login page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./App.css";
 import Tetris from "./Components/Tetris";
 import { randomTetrominos } from "./Tetrominos";
-import LoginSignup from "./Components/LoginSignup/LoginSignup";
+// import LoginSignup from "./Components/LoginSignup/LoginSignup";
 
 const App = () => {
-  return  <><LoginSignup /><Tetris /></>;
+  return  <><Tetris /></>;
 };
 
 export default App;
--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,32 +1,32 @@
-import React from "react";
-import "./LoginSignup.css";
-import email from "../../../src/Assets/email.png";
-import padlock from "../../../src/Assets/padlock.png";
-import user from "../../../src/Assets/user.png";
+// import React from "react";
+// import "./LoginSignup.css";
+// import email from "../../../src/Assets/email.png";
+// import padlock from "../../../src/Assets/padlock.png";
+// import user from "../../../src/Assets/user.png";
 
-const LoginSignup = () => {
-  return (
-    <div className="container">
-      <div className="Header">
-        <div className="text">
-          <div className="underline"></div>
-        </div>
-        <div className="inputs"></div>
-        <div className="input">
-            <img src={user} alt=''/>
-            <input type="text" placeholder="Username"/>
-        </div>
-        <div className="input">
-            <img src={email} alt=''/>
-            <input type="email" placeholder="Email"/>
-        </div>
-        <div className="input">
-            <img src={padlock} alt=''/>
-            <input type="password" placeholder="Password"/>
-        </div>
-      </div>
-    </div>
-  );
-};
+// const LoginSignup = () => {
+//   return (
+//     <div className="container">
+//       <div className="Header">
+//         <div className="text">
+//           <div className="underline"></div>
+//         </div>
+//         <div className="inputs"></div>
+//         <div className="input">
+//             <img src={user} alt='' width="20px" height="30px"/>
+//             <input type="text" placeholder="Username"/>
+//         </div>
+//         <div className="input">
+//             <img src={email} alt='' width="20px" height="30px"/>
+//             <input type="email" placeholder="Email"/>
+//         </div>
+//         <div className="input">
+//             <img src={padlock} alt='' width="20px" height="30px"/>
+//             <input type="password" placeholder="Password"/>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default LoginSignup;
+// export default LoginSignup;
