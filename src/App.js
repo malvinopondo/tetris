122fc46895109d423ef47e104a9dcf82fca44cff
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-09-13 18:36:32 +0300

    import Login component inside app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./App.css";
 import Tetris from "./Components/Tetris";
 import { randomTetrominos } from "./Tetrominos";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
+
 const App = () => {
-  return <Tetris />;
+  return  <><LoginSignup /><Tetris /></>;
 };
 
 export default App;
---
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './LoginSignup.css';
+
+const LoginSignup = () => {
+  return (
+    <div>LoginSignup</div>
+  )
+}
+
+export default LoginSignup
